3d5b44d9a9a2c1a0a75879f6f8558ce32e6088e4
manuggz <manuelggonzalezm@gmail.com>
2019-09-21 18:24:13 -0400

    Separado contenido de mmu y physical memory

--- SubSystemSimulator/src/ci4821/subsystemsimulator/hardware/MemoryManagerUnit.java
+++ SubSystemSimulator/src/ci4821/subsystemsimulator/hardware/MemoryManagerUnit.java
@@ -3,18 +3,45 @@ package ci4821.subsystemsimulator.hardware;
 import java.util.ArrayList;
 import java.util.List;
 
+import static ci4821.subsystemsimulator.hardware.PhysicalMemoryUnit.WORD_SIZE;
+
 public class MemoryManagerUnit {
 
-	List<Integer> memorySpace;
+	private static final int N_BITS_PAGES = 4; // 2 ** 4 == 16
+	private static final int PAGE_NUMBER_MASK = 61440;
+
+	private PhysicalMemoryUnit memoryRam;
 	Integer currentAddress;
 	EStatus status;
-	
-	public MemoryManagerUnit(Integer size) {
-		memorySpace = new ArrayList<>(size);
+
+	public MemoryManagerUnit() {
 		currentAddress = 0;
+		// PHYSICAL MEMORY UNIT
+		memoryRam = new PhysicalMemoryUnit();
 		status = EStatus.READY;
 	}
-	
+
+	public void set(int address, char value) {
+		int vpageNumber = (address & PAGE_NUMBER_MASK) >> WORD_SIZE - N_BITS_PAGES;
+		System.out.println("Virtual page number: " + vpageNumber);
+		// BUSCAR FRAME PAGE NUMBER EN LA PAGE TABLA QUE ESTA EN PHYSICALMEMORYUNIT vpageNumber
+		// BUSCAR EN LA TLB
+		// PUEDE OCURRIR PAGE FAULT
+		// EN CASO DE PF SO : -> PASOS SO QUE HAY QUE CREAR (swapping y algo de reemplazo)
+		// CREAR LA REAL DIRECCION EN LA MEMORIA REAL
+
+		// ESTABLECER/ OBTENER EL VALOR EN LA MEMORIA
+		// ACTUALIZAR VALORES EN LA PAGE TABLE
+
+		memoryRam.set(100,'H');
+
+	}
+
+	public char get(int address) {
+		return memoryRam.get(address);
+	}
+
+
 	enum EStatus {
 		READY, READING, WRITING, MOVING
 	}
--- SubSystemSimulator/src/ci4821/subsystemsimulator/hardware/PhysicalMemoryUnit.java
+++ SubSystemSimulator/src/ci4821/subsystemsimulator/hardware/PhysicalMemoryUnit.java
@@ -1,17 +1,36 @@
 package ci4821.subsystemsimulator.hardware;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class PhysicalMemoryUnit {
-    List<Integer> memorySpace;
+    // PHYSICAL MEMORY SIZE IN KB
+    public static final int SIZE_KB = 32;
+    // NUMBER OF BITS PER WORD
+    public static final int WORD_SIZE = 16;
+    // PAGE SIZE
+    public static final int PAGE_SIZE_KB = 4;
+    // CALCULATES THE NUMBER OF ADDRESSES THE MEMORY WILL HOLD
+    public static final int N_ADDRESSES = SIZE_KB * 1024 * 8 / WORD_SIZE;
+
+    // REPRESENTS THE PHYSICAL MEMORY
+    private char memorySpace[];
     Integer currentAddress;
     MemoryManagerUnit.EStatus status;
 
-    public PhysicalMemoryUnit(Integer size) {
-        memorySpace = new ArrayList<>(size);
+    public PhysicalMemoryUnit() {
+        memorySpace = new char[N_ADDRESSES - 1];
         currentAddress = 0;
         status = MemoryManagerUnit.EStatus.READY;
+
+        System.out.println("CREADA MEMORIA FISICA");
+        System.out.println("CON " + N_ADDRESSES + " DIRECCIONES , [0," + (N_ADDRESSES - 1) + "]"); // prints Hello World
+        System.out.println("Number of page frames: " + SIZE_KB / PAGE_SIZE_KB);
+
+    }
+    public void set(int address, char value) {
+        memorySpace[address] = value;
+    }
+
+    public char get(int address) {
+        return memorySpace[address];
     }
 
     enum EStatus {
